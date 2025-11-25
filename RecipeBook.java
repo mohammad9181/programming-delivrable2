4ce967affc6160330100a0c44924c7adf4ec6697
Mohammad Al Mousa <2583045@edu.vaniercollege.qc.ca>
2025-11-24 20:55:00 -0500

    Initial sharing of project

---
+++ RecipeBook.java
@@ -0,0 +1,38 @@
+
+/**
+ * Write a description of class RecipeBook here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class RecipeBook
+{
+    // instance variables - replace the example below with your own
+    private int x;
+    
+    private String auther;
+    private int page;
+    private int Sections;
+    private 
+
+    /**
+     * Constructor for objects of class RecipeBook
+     */
+    public RecipeBook()
+    {
+        // initialise instance variables
+        x = 0;
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     *
+     * @param  y  a sample parameter for a method
+     * @return    the sum of x and y
+     */
+    public int sampleMethod(int y)
+    {
+        // put your code here
+        return x + y;
+    }
+}
